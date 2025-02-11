d5d9a11d1c3366e30531cb60629128bcb5cc595a
Daniel Kuzmin <Den0Krik@yandex.ru>
2025-02-11 09:56:15 +0400

    ЗААЯЯЯЦ

--- CG/LAB1/Task4.py
+++ CG/LAB1/Task4.py
@@ -6,11 +6,11 @@ import numpy as np
 def build_vertex_model(vertex_array):
     matrix = np.zeros((1000, 1000), dtype = np.uint8)
     for i in range(len(vertex_array)):
-        x = round(vertex_array[i][0])
-        y = round(vertex_array[i][1])
-        matrix[y, x] = 255
+        x = round(9900 * vertex_array[i][0] + 500)
+        y = round(9900 * vertex_array[i][1] + 5)
+        matrix[x, y] = 255
 
-    return matrix
+    return np.rot90(matrix)
 
 v_array = obj_v_parser('CG\\LAB1\\data\\model_1.obj')
 matrix1 = build_vertex_model(v_array)
